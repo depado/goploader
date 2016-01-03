8b79f83641fc158e81ce221ab972f5d8c141b6ff
Depado <paul.lhussiez@gmail.com>
2016-01-03 19:03:47 +0100

    Adding progress bar capabilities, copy to clipboard and tee options

--- client/main.go
+++ client/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/atotto/clipboard"
 	"github.com/cheggaaa/pb"
+	flag "github.com/ogier/pflag"
 )
 
 const (
@@ -21,38 +23,48 @@ func main() {
 	var err error
 	var client *http.Client
 	var datasource io.Reader
-	var tee bool
 
-	flag.BoolVar(&tee, "tee", false, "Displays stdin to stdout")
+	var tee bool
+	var noprogress bool
+	var clip bool
+
+	flag.BoolVarP(&tee, "tee", "t", false, "Displays stdin to stdout")
+	flag.BoolVarP(&noprogress, "noprogress", "n", false, "Never display a progress bar")
+	flag.BoolVarP(&clip, "clipboard", "c", false, "Copy the returned URL directly to the clipboard (needs xclip or xsel)")
 	flag.Parse()
 	args := flag.Args()
 
 	if len(args) > 0 {
-		var (
-			f  *os.File
-			fi os.FileInfo
-		)
+		var f *os.File
+		var fi os.FileInfo
 
 		if f, err = os.Open(args[0]); err != nil {
 			fmt.Println("Could not open", args[0])
 			os.Exit(1)
 		}
-		if fi, err = f.Stat(); err != nil {
-			fmt.Println("Could not stat", args[0])
-			os.Exit(1)
+		if noprogress {
+			datasource = f
+		} else {
+			if fi, err = f.Stat(); err != nil {
+				fmt.Println("Could not stat", args[0])
+				os.Exit(1)
+			}
+			bar := pb.New64(fi.Size()).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+			bar.ShowPercent = true
+			bar.ShowSpeed = true
+			bar.ShowTimeLeft = true
+			bar.Start()
+			datasource = io.TeeReader(f, bar)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		bar := pb.New64(fi.Size()).SetUnits(pb.U_BYTES)
-		bar.Start()
-		datasource = io.TeeReader(f, bar)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else if tee {
-		datasource = io.TeeReader(os.Stdin, os.Stdout)
 	} else {
 		datasource = os.Stdin
 	}
-
+	if tee {
+		datasource = io.TeeReader(datasource, os.Stdout)
+	}
 	client = &http.Client{}
 	req, err := http.NewRequest(method, service, datasource)
 	resp, err := client.Do(req)
@@ -64,5 +76,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(string(ret))
+	if clip {
+		clipboard.WriteAll(string(ret))
+		fmt.Print("Copied URL to clipboard\n")
+	} else {
+		fmt.Print(string(ret))
+	}
 }
