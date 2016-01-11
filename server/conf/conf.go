ab79b06ecbb5ec075439a1ad4dfc60c8c8dedba0
Depado <paul.lhussiez@gmail.com>
2016-01-11 18:17:25 +0100

    Updating configuration settings

--- server/conf/conf.go
+++ server/conf/conf.go
@@ -8,10 +8,12 @@ import (
 
 // Conf is the struct containing the configuration of the server
 type Conf struct {
-	NameServer string `yaml:"name_server"`
-	UploadDir  string `yaml:"upload_dir"`
-	DB         string `yaml:"db"`
-	Port       int    `yaml:"port"`
+	NameServer   string `yaml:"name_server"`
+	UploadDir    string `yaml:"upload_dir"`
+	DB           string `yaml:"db"`
+	Port         int    `yaml:"port"`
+	UniURILength int    `yaml:"uniuri_length"`
+	LimitSize    int64  `yaml:"limit_size"`
 }
 
 // C is the exported global configuration variable
