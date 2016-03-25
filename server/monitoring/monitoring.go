a10226f2556372e8aca05cb760bebf7937a53cb0
Depado <paul.lhussiez@gmail.com>
2016-03-25 11:49:12 +0100

    Fixing monitoring and stats

--- server/monitoring/monitoring.go
+++ server/monitoring/monitoring.go
@@ -27,33 +27,39 @@ func Monit() {
 		var sizeRemoved uint64
 		err = database.DB.Update(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("resources"))
-			return b.ForEach(func(k, v []byte) error {
+			err = b.ForEach(func(k, v []byte) error {
 				var err error
 				r := &models.Resource{}
 				if err = r.Decode(v); err != nil {
 					return err
 				}
 				if r.DeleteAt.Before(now) {
-					found++
 					if err = b.Delete([]byte(r.Key)); err != nil {
-						return err
+						logger.Err("monitoring", "While deleting entry (skipped)", err)
+					} else {
+						if err = os.Remove(path.Join(conf.C.UploadDir, r.Key)); err != nil {
+							logger.Err("monitoring", "While deleting file (skipped)", err)
+						} else {
+							found++
+							sizeRemoved += uint64(r.Size)
+						}
 					}
-					if err = os.Remove(path.Join(conf.C.UploadDir, r.Key)); err != nil {
-						return err
-					}
-					sizeRemoved += uint64(r.Size)
-				}
-				if found > 0 {
-					var data []byte
-					models.S.CurrentFiles -= uint64(found)
-					models.S.CurrentSize -= sizeRemoved
-					if data, err = models.S.Encode(); err != nil {
-						return err
-					}
-					return tx.Bucket([]byte("statistics")).Put([]byte("main"), data)
 				}
 				return nil
 			})
+			if err != nil {
+				return err
+			}
+			if found > 0 {
+				var data []byte
+				models.S.CurrentFiles -= uint64(found)
+				models.S.CurrentSize -= sizeRemoved
+				if data, err = models.S.Encode(); err != nil {
+					return err
+				}
+				return tx.Bucket([]byte("statistics")).Put([]byte("main"), data)
+			}
+			return nil
 		})
 		if err != nil {
 			logger.Err("monitoring", "While monitoring", err)
@@ -62,6 +68,7 @@ func Monit() {
 				logger.Info("monitoring", fmt.Sprintf("Deleted %d entries and files in %s", found, time.Since(now)))
 				logger.Info("monitoring", fmt.Sprintf("Serving %d (%s) files", models.S.CurrentFiles, utils.HumanBytes(models.S.CurrentSize)))
 			}
+			logger.Debug("monitoring", fmt.Sprintf("Serving %d (%s) files", models.S.CurrentFiles, utils.HumanBytes(models.S.CurrentSize)))
 		}
 		logger.Debug("monitoring", fmt.Sprintf("Done Monit on Resources (%s)", time.Since(now)))
 		<-tc.C
