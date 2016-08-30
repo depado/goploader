b5aed3aa4964acf5a1271749d318cdc6c2ab3105
Depado <paul.lhussiez@gmail.com>
2016-08-30 22:41:31 +0200

    More screenshot programs supported

--- client/screenshot/screenshot.go
+++ client/screenshot/screenshot.go
@@ -5,21 +5,61 @@ import (
 	"os/exec"
 )
 
+// Program is a simple struct defining a screenshot program
+type Program struct {
+	Command        string
+	WindowArgs     []string
+	FullScreenArgs []string
+}
+
+// GetCommand returns an *exec.Cmd
+func (p *Program) GetCommand(fp string, window bool) *exec.Cmd {
+	var args []string
+	if window {
+		args = append(args, p.WindowArgs...)
+		args = append(args, fp)
+	} else {
+		args = append(args, p.FullScreenArgs...)
+		args = append(args, fp)
+	}
+	return exec.Command(p.Command, args...)
+}
+
+// Do executes a screenshot with the given program
+func (p *Program) Do(fp string, window bool) error {
+	cmd := p.GetCommand(fp, window)
+	cmd.Stderr = nil
+	cmd.Stdout = nil
+	return cmd.Run()
+}
+
+var progs = []Program{
+	{
+		Command:        "gnome-screenshot",
+		WindowArgs:     []string{"-a", "-f"},
+		FullScreenArgs: []string{"-f"},
+	},
+	{
+		Command:        "import",
+		WindowArgs:     []string{},
+		FullScreenArgs: []string{"-window", "root"},
+	},
+	{
+		Command:        "scrot",
+		WindowArgs:     []string{"-s"},
+		FullScreenArgs: []string{},
+	},
+}
+
 // Do executes the screenshot, saving the file to the file path fp.
 func Do(fp string, window bool) error {
-	_, err := exec.LookPath("import")
-	if err != nil {
-		return fmt.Errorf("Command 'import' is not installed.")
-	}
-	var cmd *exec.Cmd
-	if window {
-		cmd = exec.Command("import", fp)
-	} else {
-		cmd = exec.Command("import", "-window", "root", fp)
-	}
-	cmd.Stdout = nil
-	cmd.Stderr = nil
+	var err error
 
-	err = cmd.Run()
-	return err
+	for _, p := range progs {
+		if _, err = exec.LookPath(p.Command); err == nil {
+			return p.Do(fp, window)
+		}
+	}
+
+	return fmt.Errorf("No screenshot program found")
 }
