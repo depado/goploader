e1252f1a955c24b5348fed794483216361f494f3
Depado <paul.lhussiez@gmail.com>
2017-10-15 15:39:08 +0200

    Migrate func to migrate from bolt to storm

--- server/models/initialize.go
+++ server/models/initialize.go
@@ -1,7 +1,11 @@
 package models
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/asdine/storm"
+	"github.com/boltdb/bolt"
 
 	"github.com/Depado/goploader/server/database"
 	"github.com/Depado/goploader/server/logger"
@@ -14,6 +18,7 @@ func Initialize() error {
 	if err = database.DB.Init(&Resource{}); err != nil {
 		logger.Fatal("server", "Couldn't initialize bucket", err)
 	}
+	Migrate()
 	if err = S.Load(); err != nil {
 		switch err {
 		case storm.ErrNotFound:
@@ -28,4 +33,52 @@ func Initialize() error {
 	S.Info()
 	logger.Debug("server", "Done Initialize on statistics object")
 	return err
+}
+
+// Migrate migrates the old data format to the new one
+func Migrate() {
+	var err error
+	type us struct {
+		TotalSize    uint64
+		TotalFiles   uint64
+		CurrentSize  uint64
+		CurrentFiles uint64
+	}
+	var ss us
+	if err = database.DB.Get("statistics", "main", &ss); err == nil {
+		S.CurrentFiles = ss.CurrentFiles
+		S.CurrentSize = ss.CurrentSize
+		S.TotalFiles = ss.TotalFiles
+		S.TotalSize = ss.TotalSize
+		if err = S.Save(); err != nil {
+			logger.Err("server", "Couldn't migrate old stats", err)
+			return
+		}
+		logger.Info("server", "Had to migrate old stats")
+		database.DB.Delete("statistics", "main")
+		database.DB.Drop("statistics")
+	}
+
+	var rr []Resource
+	var r Resource
+	err = database.DB.Bolt.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("resources"))
+		if b == nil {
+			return fmt.Errorf("no bucket")
+		}
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			json.Unmarshal(v, &r)
+			r.UnixDeleteAt = r.DeleteAt.Unix()
+			rr = append(rr, r)
+		}
+		return nil
+	})
+	if err == nil {
+		for _, r := range rr {
+			database.DB.Save(&r)
+		}
+		logger.Info("server", "Had to migrate old resources")
+		database.DB.Drop("resources")
+	}
 }
